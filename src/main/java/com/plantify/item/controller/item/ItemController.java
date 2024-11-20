97cc4dfb5499d429274957a0fa06c426e9010f3a
zzoo0123 <0123suh@gmail.com>
2024-11-20 17:42:02 +0900

    fix: merge error

--- src/main/java/com/plantify/item/controller/item/ItemController.java
+++ src/main/java/com/plantify/item/controller/item/ItemController.java
@@ -1,9 +1,9 @@
-package com.plantify.item.controller;
+package com.plantify.item.controller.item;
 
 import com.plantify.item.domain.dto.request.ItemRequest;
 import com.plantify.item.domain.dto.response.ItemResponse;
 import com.plantify.item.global.response.ApiResponse;
-import com.plantify.item.service.ItemService;
+import com.plantify.item.service.item.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/plantify/item/domain/dto/request/MyItemUserRequest.java
+++ src/main/java/com/plantify/item/domain/dto/request/MyItemUserRequest.java
@@ -3,20 +3,13 @@ package com.plantify.item.domain.dto.request;
 import com.plantify.item.domain.entity.Item;
 import com.plantify.item.domain.entity.MyItem;
 
-<<<<<<< HEAD:src/main/java/com/plantify/item/domain/dto/request/MyItemRequest.java
-public record MyItemRequest(Long itemId, Status status) {
 
-    public MyItem toEntity(Long userId, Item item) {
-        return MyItem.builder()
-                .userId(userId)
-=======
 public record MyItemUserRequest(
         Long itemId,
         Long userId
 ) {
     public MyItem toEntity(Item item) {
         return MyItem.builder()
->>>>>>> feat/myItem-service:src/main/java/com/plantify/item/domain/dto/request/MyItemUserRequest.java
                 .item(item)
                 .userId(userId)
                 .build();
