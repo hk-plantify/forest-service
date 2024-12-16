fbd088a3b08c3ca407a579df94d4b33540ae79d7
seunzu <0123suh@gmail.com>
2024-12-17 03:23:20 +0900

    feat: myItemId 추가

--- src/main/java/com/plantify/item/domain/dto/response/GroupedMyItemWithIdsResponse.java
+++ src/main/java/com/plantify/item/domain/dto/response/GroupedMyItemWithIdsResponse.java
@@ -4,6 +4,7 @@ import com.plantify.item.domain.entity.Category;
 import com.plantify.item.domain.entity.MyItem;
 
 public record GroupedMyItemWithIdsResponse(
+        Long myItemId,
         Long itemId,
         String itemName,
         String imageUri,
@@ -12,6 +13,7 @@ public record GroupedMyItemWithIdsResponse(
 ) {
     public static GroupedMyItemWithIdsResponse  from(MyItem myItem) {
         return new GroupedMyItemWithIdsResponse(
+                myItem.getMyItemId(),
                 myItem.getMyItemId(),
                 myItem.getItem().getName(),
                 myItem.getItem().getImageUri(),
